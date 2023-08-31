cbdea9a076e14104378bd64e1b7e3c7d95056f44
lehuy299 <57005623+lehuy299@users.noreply.github.com>
2023-08-31 18:11:48 +0700

    sed commit

--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -2,11 +2,26 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomersService {
-    findCustomer() {
-        return {
+
+    private users = [
+        {
             id: 1,
             email: 'danny@gmail.com',
             createAt: new Date()
+        },
+        {
+            id: 2,
+            email: 'adam@gmail.com',
+            createAt: new Date()
+        },
+        {
+            id: 3,
+            email: 'spencer@gmail.com',
+            createAt: new Date()
         }
+    ]
+
+    findCustomerById(id: number) {
+        return this.users.find(user => user.id === id);
     }
 }
